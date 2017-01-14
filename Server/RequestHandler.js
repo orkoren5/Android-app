c2bcc926af1de98890f6740cb5e043cbc873a9f3
Koren <or.koren@sap.com>
2017-01-14 14:54:27 +0200

    develop server

---
+++ Server/RequestHandler.js
@@ -0,0 +1,160 @@
+var MongoClient = require('mongodb').MongoClient;
+var ObjectID = require('mongodb').ObjectID;
+var url = 'mongodb://localhost:27017/homie';
+var jwt = require('jsonwebtoken');
+
+function getColName(sEntityName) {
+	return sEntityName;
+}
+
+function checkValidity(sEntityName, oObject) {
+	return true;
+}
+
+function convertHexToObjectID(sHex) {	
+	try {
+		return ObjectID.createFromHexString(sObjectId);
+	} catch (err) {
+		return null;
+	}
+}
+
+var handleLoginRequest = function(oUser, secretKey, responseStream) {
+		MongoClient.connect(url, function(err, db) {
+		var col = db.collection("users");
+		col.findOne({name: oUser.name}, function(err, user) {
+			if (err) 
+				throw err;
+		    if (!user || user.password != oUser.password) {
+		      responseStream.json({ success: false, message: 'Authentication failed. Incorrect username or password.' });
+		    } else {
+
+		        // create a token
+		        var token = jwt.sign(user, secretKey, {
+		          expiresIn: "24h" // expires in 24 hours
+		        });		    
+		        responseStream.json({
+		          success: true,
+		          message: 'Enjoy your token!',
+		          token: token
+		        });
+	      	}
+	      	db.colse();
+	  	});
+	});
+};
+
+var handleSignUpRequest = function(oUser, secretKey, responseStream) {
+	MongoClient.connect(url, function(err, db) {	
+		var col = db.collection("users");
+		if (checkValidity("users", oUser)) {
+			oUser.groupIds = [];
+			oUser.assignmentIds = [];
+			col.insertOne(oUser, function(err, result) {
+
+				 // create a token
+		        var token = jwt.sign(oUser, secretKey, {
+		          expiresIn: "24h" // expires in 24 hours
+		        });		    
+		        responseStream.json({
+		          success: true,
+		          message: 'Enjoy your token!',
+		          token: token
+		        });
+			    db.close();
+			});
+		}	
+	});
+};
+
+var handleGetByIdRequest = function(sEntityName, sId, responseStream) {
+	var colName = getColName(sEntityName);
+	MongoClient.connect(url, function(err, db) {
+		var col = db.collection(colName);
+		col.findOne({_id: convertHexToObjectID(sObjectId)}, function(err, item) {
+			responseStream.json(item);
+		    db.close();
+	  	});
+	});
+};
+
+var handleGetRequest = function(sEntityName, req, responseStream) {
+	var colName = getColName(sEntityName);
+	var query = req.query;
+	MongoClient.connect(url, function(err, db) {
+		var col = db.collection(colName);
+		if (Object.getOwnPropertyNames(query).length === 0) {
+			query.owner = req.user._id.toString();
+		}
+		col.find(query).toArray(function(err, items) {
+			responseStream.json(items);
+		    db.close();
+	  	});
+	});
+};
+
+
+var handlePostRequest = function(sEntityName, req, responseStream) {
+	var oObject = req.body;
+	var colName = getColName(sEntityName);
+	MongoClient.connect(url, function(err, db) {	
+		var col = db.collection(colName);
+		if (checkValidity(colName, oObject)) {
+			oObject.owner = req.user._id.toString();
+			col.insertOne(oObject, function(err, result) {
+				responseStream.send(oObject._id.toString());
+				console.log(oObject._id);
+			    db.close();
+			});
+		}	
+	});
+};
+
+var handlePutRequest = function(sEntityName, req, responseStream) {
+	var oFieldsToUpdate = req.body,
+		colName = getColName(sEntityName);
+		sObjectId = req.params.id;
+	MongoClient.connect(url, function(err, db) {	
+		var col = db.collection(colName);
+		if (checkValidity(colName, oFieldsToUpdate)) {
+			col.update({_id: convertHexToObjectID(sObjectId)}, {
+				$set: oFieldsToUpdate,
+				$currentDate: { "lastModified": true }
+			},	function(err, result) {
+				var message = result.result.nModified > 0 
+					? "Object was modified successfully" 
+					: "Object modification failed - object ID does not exist";
+				var status = result.result.nModified > 0 ? 200 : 400;
+				responseStream.status(status).send(message);
+				console.log(status + ": " + message);
+			    db.close();
+			});
+		}	
+	});
+};
+
+var handleDeleteRequest = function(sEntityName, req, responseStream) {
+	var colName = getColName(sEntityName),
+		sObjectId = req.params.id,
+		sOwnerId = req.user._id.toString();
+	MongoClient.connect(url, function(err, db) {	
+		var col = db.collection(colName);
+		col.remove({_id: convertHexToObjectID(sObjectId), owner: convertHexToObjectID(sOwnerId)},	function(err, result) {
+			var message = result.result.n > 0 
+				? "Object was deleted successfully" 
+				: "Object deletion failed - object ID does not exist";
+			var status = result.result.n > 0 ? 200 : 400;
+			responseStream.status(status).send(message);
+			console.log(status + ": " + message);
+		    db.close();
+		});		
+	});
+};
+
+exports.handleGetByIdRequest = handleGetByIdRequest;
+exports.handleGetRequest = handleGetRequest;
+exports.handlePostRequest = handlePostRequest;
+exports.handlePutRequest = handlePutRequest;
+exports.handleDeleteRequest = handleDeleteRequest;
+exports.handleLoginRequest = handleLoginRequest;
+exports.handleSignUpRequest = handleSignUpRequest;
--- Server/main.js
+++ Server/main.js
@@ -1,15 +1,156 @@
-var http = require("http");
+//r request = require('request');
+var url = require('url');
+var express = require('express');
+var bodyParser = require('body-parser')
+var jwt = require('jsonwebtoken');
 
-http.createServer(function (request, response) {
+var app = express();
+var apiRoutes = express.Router(); 
 
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body as "Hello World"
-   response.end('Hello World\n');
-}).listen(8081);
+var handler = require("./RequestHandler");
 
-// Console will print the message
+app.use(bodyParser.json());
+
+//////////////////////////////////////////////////////////
+// POST authentication enpoints (login | logout | signup)
+//////////////////////////////////////////////////////////
+app.set('secretKey', 'coolhomieapp');
+
+app.post('/login', function(req, response) {
+	handler.handleLoginRequest(req.body, app.get('secretKey'), response);
+});
+
+app.post('/signup', function(req, response) {
+	handler.handleSignUpRequest(req.body, app.get('secretKey'), response);
+});
+
+app.get('/logout', function(req, response) {
+
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+	response.write(req.params.username);
+	response.end();
+});
+
+//////////////////////////////////////////////////////////
+
+apiRoutes.use(function(req, res, next) {
+
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['token'];
+
+  // decode token
+  if (token) {
+
+    // verifies secret and checks exp
+    jwt.verify(token, app.get('secretKey'), function(err, decoded) {      
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });    
+      } else {
+        req.user = decoded;    
+        next();
+      }
+    });
+  } else {
+
+    // if there is no token, return 403 error
+    return res.status(403).send({ 
+        success: false, 
+        message: 'No token provided.' 
+    });    
+  }
+});
+
+//////////////////////////////////////////////////////////
+// GET request enpoints
+//////////////////////////////////////////////////////////
+
+apiRoutes.get('/courses/:id', function(req, response) {
+	handler.handleGetByIdRequest("courses", req.params.id, response);
+});
+
+apiRoutes.get('/courses', function(req, response) {
+	handler.handleGetRequest("courses", req, response);
+});
+
+apiRoutes.get('/assignments/:id', function(req, response) {
+	handler.handleGetByIdRequest("assignments", req.params.id, response);
+});
+
+apiRoutes.get('/assignments', function(req, response) {
+	handler.handleGetRequest("assignments", req, response);
+});
+
+apiRoutes.get('/groups/:id', function(req, response) {
+	handler.handleGetByIdRequest("groups", req.params.id, response);
+});
+
+apiRoutes.get('/groups', function(req, response) {
+	handler.handleGetRequest("groups", req, response);
+});
+
+apiRoutes.get('/users/:id', function(req, response) {
+	handler.handleGetByIdRequest("users", req.params.id, response);
+});
+
+apiRoutes.get('/tasks/:id', function(req, response) {
+	handler.handleGetByIdRequest("tasks", req.params.id, response);
+});
+
+apiRoutes.get('/tasks', function(req, response) {
+	handler.handleGetRequest("tasks", req, response);
+});
+
+//////////////////////////////////////////////////////////
+// POST request enpoints
+//////////////////////////////////////////////////////////
+
+apiRoutes.post('/users', function(req, response) {
+	handler.handlePostRequest("users", req, response);
+});
+
+apiRoutes.post('/groups', function(req, response) {
+	handler.handlePostRequest("groups", req, response);
+});
+
+apiRoutes.post('/tasks', function(req, response) {
+	handler.handlePostRequest("tasks", req, response);
+});
+
+apiRoutes.post('/assignments', function(req, response) {
+	handler.handlePostRequest("tasks", req, response);
+});
+
+
+//////////////////////////////////////////////////////////
+// PUT request enpoints
+//////////////////////////////////////////////////////////
+
+apiRoutes.put('/users/:id', function(req, response) {
+	handler.handlePutRequest("users", req, response);
+});
+
+apiRoutes.put('/groups/:id', function(req, response) {
+	handler.handlePutRequest("groups", req, response);
+});
+
+apiRoutes.put('/tasks/:id', function(req, response) {
+	handler.handlePutRequest("tasks", req, response);
+});
+
+apiRoutes.put('/assignments/:id', function(req, response) {
+	handler.handlePutRequest("assignments", req, response);
+});
+
+//////////////////////////////////////////////////////////
+// DELETE request enpoints
+//////////////////////////////////////////////////////////
+
+apiRoutes.delete('/groups/:id', function(req, response) {
+	handler.handleDeleteRequest("groups", req, response);
+});
+
+app.use('/api', apiRoutes);
+
+app.listen(8081);
+
 console.log('Server running at http://127.0.0.1:8081/');
